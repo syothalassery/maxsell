215b5f4274c41890fa5e21d79386186c6937cb6d
syothalassery <syothalasseysys@gmail.com>
2018-05-07 09:26:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {MatPaginator,MatTableDataSource, MatSort, MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  displayedColumns = ['slno','name','groupName','brandName','tax','openingStock','minStock','currentStock','action'];
+  dataSource = new MatTableDataSource<Item>(ELEMENT_DATA);
+  newItem:Item;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(public snackBar: MatSnackBar) {}
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.newItem = new Item();
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  addItem(){
+    if(this.newItem.slno == null){
+      this.newItem.slno = (this.dataSource.data.length!=0)?this.dataSource.data.length+1:1;
+      this.dataSource.data.push(this.newItem);
+    }
+    this.newItem = new Item();
+    this.dataSource._updateChangeSubscription();
+    this.snackBar.open("Items Updated Successfully","close", {
+      duration: 2000,
+    });
+  }
+  editItem(element){
+    this.newItem = new Item();
+    this.newItem = element;
+  }
+  deleteItem(element){
+    this.dataSource.data.map((val,i)=>{
+      if(element.slno == val.slno){
+        this.dataSource.data.splice(i,1);
+      }
+    })
+    this.dataSource._updateChangeSubscription();
+    this.snackBar.open("Items Deleted Successfully","close", {
+      duration: 2000,
+    });
+  }
 }
+export class Item {
+  slno: number;
+  name: string;
+  groupName: string;
+  brandName: string;
+  tax:number;
+  openingStock:number;
+  minStock:number;
+  currentStock:number;
+}
+
+const ELEMENT_DATA: Item[] = [
+  {slno: 1, name: 'Baby Food', groupName: "Baby Food/Items", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 2, name: 'Diapers', groupName: "Baby Food/Items", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 3, name: 'Formula', groupName: "Baby Food/Items", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 4, name: 'Bagels', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 5, name: 'Bread', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 6, name: 'Cookies', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 7, name: 'Croissants', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 8, name: 'Donuts', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 9, name: 'Hot Dog Buns', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 10, name: 'Rolls', groupName: "Bakery", brandName: 'Food bazaar', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 11, name: 'Aluminum Foil', groupName: "Kitchen", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 12, name: 'Non-stick Spray', groupName: "Kitchen", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 13, name: 'Paper Towels', groupName: "Kitchen", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 14, name: 'Plastic Wrap', groupName: "Kitchen", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 15, name: 'Sandwich Bags', groupName: "Kitchen", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 16, name: 'Elbows', groupName: "Pasta/Sauce", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 17, name: 'Grated Parmesan', groupName: "Pasta/Sauce", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 18, name: 'Red Pasta Sauce', groupName: "Pasta/Sauce", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 19, name: 'Spaghetti Noodles', groupName: "Pasta/Sauce", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+  {slno: 20, name: 'White Pasta Sauce', groupName: "Pasta/Sauce", brandName: 'Galaxy', tax:5, openingStock:1000, minStock:10,currentStock:100},
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSnackBarModule,MatButtonModule,MatFormFieldModule, MatTooltipModule, MatCardModule, MatTableModule, MatInputModule, MatCheckboxModule,  MatNativeDateModule, MatPaginatorModule, MatSortModule} from '@angular/material';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    AngularFontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
